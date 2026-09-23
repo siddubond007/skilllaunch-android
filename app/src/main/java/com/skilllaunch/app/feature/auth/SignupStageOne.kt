4d83bc4947c5f1ad632019319670d140987fb6dc
bond007always <siddusiddharth80193@gmail.com>
2026-09-23 18:28:37 +0530

    fix: correct auth ui imports

--- app/src/main/java/com/skilllaunch/app/feature/auth/SignupStageOne.kt
+++ app/src/main/java/com/skilllaunch/app/feature/auth/SignupStageOne.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -31,6 +30,7 @@ import androidx.compose.ui.text.input.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.text.KeyboardOptions
 
 @Composable
 fun SignupStageOne(
