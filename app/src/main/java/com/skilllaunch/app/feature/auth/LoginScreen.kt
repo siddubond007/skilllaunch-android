65bbf630cd0f76d5e9d67f03e2218128cf1d625e
bond007always <siddusiddharth80193@gmail.com>
2026-09-23 17:25:45 +0530

    fix: add login action import

--- app/src/main/java/com/skilllaunch/app/feature/auth/LoginScreen.kt
+++ app/src/main/java/com/skilllaunch/app/feature/auth/LoginScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.shape.RoundedCornerShape
