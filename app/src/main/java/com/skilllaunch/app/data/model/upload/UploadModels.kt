bde815c5225ecb053d54ef19c3437ae1367e279d
bond007always <siddusiddharth80193@gmail.com>
2026-09-24 15:27:10 +0530

    feat: add resume upload response model

---
+++ app/src/main/java/com/skilllaunch/app/data/model/upload/UploadModels.kt
@@ -0,0 +1,8 @@
+package com.skilllaunch.app.data.model.upload
+
+data class ResumeUploadResponse(
+    val message: String? = null,
+    val url: String? = null,
+    val fileName: String? = null,
+    val bytes: Long? = null
+)
