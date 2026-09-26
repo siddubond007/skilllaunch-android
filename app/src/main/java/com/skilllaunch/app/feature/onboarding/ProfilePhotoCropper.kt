95769b29887fe36194676af6c0d5c96ad9b69b59
bond007always <siddusiddharth80193@gmail.com>
2026-09-26 22:23:18 +0530

    fix: add cropper button width import

--- app/src/main/java/com/skilllaunch/app/feature/onboarding/ProfilePhotoCropper.kt
+++ app/src/main/java/com/skilllaunch/app/feature/onboarding/ProfilePhotoCropper.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
